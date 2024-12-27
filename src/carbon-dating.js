ddea3fa74920a853a27b42a9ef79cecccc154098
whowouldwin <leranetwork@gmail.com>
2024-12-28 01:15:22 +0300

    feat: implement dateSample

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,13 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (typeof sampleActivity !==  "string" || isNaN(sampleActivity) || +sampleActivity > MODERN_ACTIVITY || +sampleActivity <= 0) {
+    return false;
+  }
+  const activity = parseFloat(sampleActivity);
+  const k = Math.LN2 / HALF_LIFE_PERIOD;
+  return Math.ceil(1 / k * Math.log(MODERN_ACTIVITY / activity));
 }
 
 module.exports = {
