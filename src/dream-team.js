bf89e9c8bc09f34722af94543088f4816e229835
whowouldwin <leranetwork@gmail.com>
2024-12-29 19:29:19 +0300

    feat: implement createDreamTeam

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  if (!Array.isArray(members)) {
+    return false;
+  }
+  let names = [];
+  members.forEach((member) => {
+    if (typeof member === 'string') {
+      names.push(member.trim()[0].toUpperCase());
+    }
+  })
+  return names.sort().join('');
 }
 
 module.exports = {
