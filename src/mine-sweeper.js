fe8dfea0a7c24cc2a8a613280f21ae052555feeb
whowouldwin <leranetwork@gmail.com>
2025-01-06 03:11:20 +0300

    feat: implement minesweeper

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const {NotImplementedError} = require('../extensions/index.js');
 
 /**
  * In the popular Minesweeper game you have a board with some mines and those cells
@@ -23,9 +23,43 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  const totalRows = matrix.length;
+  const totalColumns = matrix[0].length;
+
+  const minesweeperBoard = Array.from({ length: totalRows }, () => Array(totalColumns).fill(0));
+
+  function countMines(currentRow, currentColumn) {
+    let count = 0;
+    for (let rowOffset = -1; rowOffset <= 1; rowOffset++) {
+      for (let columnOffset = -1; columnOffset <= 1; columnOffset++) {
+        const neighborRow = currentRow + rowOffset;
+        const neighborColumn = currentColumn + columnOffset;
+
+        if (neighborRow >= 0 &&
+          neighborRow < totalRows &&
+
+          neighborColumn >= 0 &&
+          neighborColumn < totalColumns &&
+
+          (rowOffset !== 0 || columnOffset !== 0) &&
+          matrix[neighborRow][neighborColumn]) {
+
+          count++;
+        }
+
+      }
+    }
+    return count;
+  }
+
+  for (let i = 0; i < totalRows; i++) {
+    for (let j = 0; j < totalColumns; j++) {
+      minesweeperBoard[i][j] = countMines(i, j);
+    }
+  }
+
+  return minesweeperBoard;
 }
 
 module.exports = {
